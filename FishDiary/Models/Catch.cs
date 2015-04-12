a3ace26be9c1eb521362a7a8224a1f53075da3f6
Kalle Häkkänen <kalle.hakkanen@gmail.com>
2015-04-12 09:51:53 +0300

    Added migrations, added Catch.cs

---
+++ FishDiary/Models/Catch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FishDiary.Models
+{
+    public class Catch
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public int? CatchPictureID { get; set; }
+        public DateTime TimeOfCatch { get; set; }
+        public string Location { get; set; }
+        public string LatCoordinates { get; set; }
+        public string LonCoordinates { get; set; }
+        public float Depth { get; set; }
+        public string FishName { get; set; }
+        public float Length { get; set; }
+        public float Weight { get; set; }
+        public string Lure { get; set; }
+        public string Bait { get; set; }
+        public string Notes { get; set; }
+        public bool Public { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+        public virtual CatchPicture CatchPicture { get; set; }
+    }
+}
